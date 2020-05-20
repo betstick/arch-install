04149fc9077bd1e07ad64562dee4779bbf239c0d
betstick <61669265+betstick@users.noreply.github.com>
2020-05-20 14:33:00 -0400

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Install to which disk? (Use full disk path e.g. /dev/sda)"
+read diskname
+#make the disk a GPT disk
+echo "Zeroing disk..."
+dd if=/dev/zero of=$diskname count=1024
+echo "Disk zeroed"
+echo "Making disk GPT..."
+parted -a optimal $diskname mklabel GPT
+echo "Creating UEFI partition..."
+parted -a optimal $diskname mkpart primary 0% 512MB
+echo "Creating root partition..."
+parted $diskname mkpart primary ext4  512
