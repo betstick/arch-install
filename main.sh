418fde8219afbfe5de9d0b17d1c635fdf924ae0b
betstick <61669265+betstick@users.noreply.github.com>
2020-05-20 15:08:51 -0400

    fixed something

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+#
+#disk stuff
+#
 echo "Install to which disk? (Use full disk path e.g. /dev/sda)"
 read diskname
 #make the disk a GPT disk
@@ -11,3 +15,10 @@ echo "Creating UEFI partition..."
 parted -a optimal $diskname mkpart primary 0% 512MB
 echo "Creating root partition..."
 parted -a optimal $diskname mkpart primary 512 100%
+disk1 = "${diskname}1"
+disk2 = "${diskname}2"
+mkfs.ext4 $disk2
+mkfs.fat -F32 $disk1
+mount $disk2 /mnt
+mkdir /mnt/boot
+mount $disk1 /mnt/boot
