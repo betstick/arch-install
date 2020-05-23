342bacf19f557aa0efdf50c1499776b67ac8fa4d
betstick <61669265+betstick@users.noreply.github.com>
2020-05-23 18:23:43 -0400

    changed password commands

--- full-auto.sh
+++ full-auto.sh
@@ -12,7 +12,7 @@ mount /dev/vda1 /mnt/boot
 pacstrap /mnt base linux linux-firmware dhcpcd netctl grub efibootmgr nano
 genfstab -U /mnt >> /mnt/etc/fstab
 echo -e "127.0.0.1	localhost\n::1	localhost\n127.0.1.1	autoarch.localdomain	autoarch" > /mnt/etc/hosts
-echo autoarch > /etc/hostname
+echo autoarch > /mnt/etc/hostname
 echo -e "Interface=enp1s0\nConnection=ethernet\nIP=dhcp" > /mnt/etc/netctl/main
 #listen, it was either this nonsense, or i had to create a second script...
 arch-chroot /mnt netctl enable main
@@ -21,4 +21,4 @@ arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/ --bootl
 #arch-chroot /mnt mkinitcpio -P
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt mkinitcpio -P #hehe
-arch-chroot /mnt echo "root:passwd" | chpasswd
+arch-chroot /mnt echo '"root:password" | chpasswd'
