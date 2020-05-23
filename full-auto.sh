2c49df7a6efe6f82ad70d7114ace7e5c92e9bf79
betstick <61669265+betstick@users.noreply.github.com>
2020-05-23 19:56:57 -0400

    added serial setup

--- full-auto.sh
+++ full-auto.sh
@@ -9,7 +9,7 @@ mkfs.ext4 /dev/vda2
 mount /dev/vda2 /mnt
 mkdir /mnt/boot
 mount /dev/vda1 /mnt/boot
-pacstrap /mnt base linux linux-firmware dhcpcd netctl grub efibootmgr nano
+pacstrap /mnt base linux linux-firmware dhcpcd netctl grub efibootmgr nano bash-completion
 genfstab -U /mnt >> /mnt/etc/fstab
 echo -e "127.0.0.1	localhost\n::1	localhost\n127.0.1.1	autoarch.localdomain	autoarch" > /mnt/etc/hosts
 echo autoarch > /mnt/etc/hostname
@@ -18,10 +18,12 @@ echo -e "Interface=enp1s0\nConnection=ethernet\nIP=dhcp" > /mnt/etc/netctl/main
 arch-chroot /mnt netctl enable main
 arch-chroot /mnt systemctl enable dhcpcd
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=ARCH
-#arch-chroot /mnt mkinitcpio -P
+#enable serial console
+echo -e 'GRUB_TERMINAL_INPUT="console serial"' >> /mnt/etc/default/grub
+echo -e 'GRUB_TERMINAL_OUTPUT="gfxterm serial"' >> /mnt/etc/default/grub
+echo -e 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200"' >> /mnt/etc/default/grub
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt mkinitcpio -P #hehe
-#arch-chroot /mnt eval 'echo "root:password" | chpasswd'
 echo -e 'echo "root:password" | chpasswd' > /mnt/password
 chmod +x /mnt/password
 arch-chroot /mnt ./password
