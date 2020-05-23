1a67932c3741af815cb23358af213166e87f8077
betstick <61669265+betstick@users.noreply.github.com>
2020-05-23 18:10:17 -0400

    enabled dhcp service

--- full-auto.sh
+++ full-auto.sh
@@ -16,6 +16,7 @@ echo autoarch > /etc/hostname
 echo -e "Interface=enp1s0\nConnection=ethernet\nIP=dhcp" > /mnt/etc/netctl/main
 #listen, it was either this nonsense, or i had to create a second script...
 arch-chroot /mnt netctl enable main
+arch-chroot /mnt systemctl enable dhcpcd
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=ARCH
 arch-chroot /mnt mkinitcpio -P
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
