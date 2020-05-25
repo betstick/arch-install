3afb687cfe56b9febb7d4edebb10cf6ba413f293
betstick <61669265+betstick@users.noreply.github.com>
2020-05-25 14:50:21 -0400

    reworked script to run outside vm and create disk image for vm

--- full-auto.sh
+++ full-auto.sh
@@ -1,30 +1,77 @@
 #!/bin/sh
-#this only works if you use qemu vms lul. not making this generic.
-dd if=/dev/zero of=/dev/vda count=1000000
-parted -a optimal /dev/vda mklabel GPT
-parted -a optimal /dev/vda mkpart primary 0% 512
-parted -a optimal /dev/vda mkpart primary 512 100%
-mkfs.fat -F32 /dev/vda1
-mkfs.ext4 /dev/vda2
-mount /dev/vda2 /mnt
-mkdir /mnt/boot
-mount /dev/vda1 /mnt/boot
-pacstrap /mnt base linux linux-firmware dhcpcd netctl grub efibootmgr nano bash-completion
-genfstab -U /mnt >> /mnt/etc/fstab
-echo -e "127.0.0.1	localhost\n::1	localhost\n127.0.1.1	autoarch.localdomain	autoarch" > /mnt/etc/hosts
-echo autoarch > /mnt/etc/hostname
-echo -e "Interface=enp1s0\nConnection=ethernet\nIP=dhcp" > /mnt/etc/netctl/main
+#this only works if you run it on arch lul. not making this generic. its for personal use, steal parts if you want them.
+echo "GIMME NAMES"
+read vmname
+echo "domain? (.local is a sane default)"
+read domain
+echo "disk size in MB?(2048MB IS THE MINIMUM"
+read size
+
+mkdir "$vmname"
+cd "$vmname"
+
+dir=$(pwd)
+
+#make a sparse disk of custom size
+dd if=/dev/zero of="$vmname".img  bs=1k seek="$size" count=1
+
+#partition the disks, disk can be made arbitrary size
+parted -a optimal "$vmname".img mklabel GPT
+parted -a optimal "$vmname".img mkpart primary 0% 512
+parted -a optimal "$vmname".img mkpart primary 512 100%
+
+#mount the boot dir onto a loop dev
+loop1=$(losetup -f) #we'll need this to unmount later!
+echo $loop1
+losetup -f -P --offset 1048576 --sizelimit 511704576 "$vmname".img
+
+#mount the root dir onto a loop dev
+loop2=$(losetup -f)
+echo $loop2
+losetup -f -P --offset 511705088 "$vmname".img
+
+#make the filesystems
+mkfs.fat -F32 "$loop1"
+mkfs.ext4 "$loop2"
+
+#mount everything to real directories
+mkdir mont
+mount "$loop2" mont/
+mkdir mont/boot
+mount "$loop1" mont/boot
+
+#start the actual install on our image
+pacstrap mont/ base linux linux-firmware dhcpcd netctl grub efibootmgr nano bash-completion
+genfstab -U mont/ >> mont/etc/fstab
+
+#set up the network
+echo -e "127.0.0.1	localhost\n::1	localhost\n127.0.1.1	$vmname.localdomain	$vmname" > mont/etc/hosts
+echo "$vmname" > mont/etc/hostname
+echo -e "Interface=enp1s0\nConnection=ethernet\nIP=dhcp" > mont/etc/netctl/main
+
 #listen, it was either this nonsense, or i had to create a second script...
-arch-chroot /mnt netctl enable main
-arch-chroot /mnt systemctl enable dhcpcd
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=ARCH
+arch-chroot mont/ netctl enable main
+arch-chroot mont/ systemctl enable dhcpcd
+arch-chroot mont/ grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=ARCH
+
 #enable serial console
-echo -e 'GRUB_TERMINAL_INPUT="console serial"' >> /mnt/etc/default/grub
-echo -e 'GRUB_TERMINAL_OUTPUT="gfxterm serial"' >> /mnt/etc/default/grub
-echo -e 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200"' >> /mnt/etc/default/grub
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-arch-chroot /mnt mkinitcpio -P #hehe
-echo -e 'echo "root:password" | chpasswd' > /mnt/password
-chmod +x /mnt/password
-arch-chroot /mnt ./password
-rm /mnt/password
+echo -e 'GRUB_TERMINAL_INPUT="console serial"' >> mont/etc/default/grub
+echo -e 'GRUB_TERMINAL_OUTPUT="gfxterm serial"' >> mont/etc/default/grub
+echo -e 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200"' >> mont/etc/default/grub
+
+#finish grub setup and initramfs
+arch-chroot mont/ grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot mont/ mkinitcpio -P #hehe
+
+#set root password to password, im lazy
+echo -e 'echo "root:password" | chpasswd' > mont/password
+chmod +x mont/password
+arch-chroot mont/ ./password
+rm mont/password
+#install is done.
+
+#cleanup time!
+#release the loops and delete mont
+umount "$vmname"/mont/boot
+umount "$vmname"/mont
+rm -rf mont
